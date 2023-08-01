69591ef616cae039f352ae30ee9fc4f0bf05af65
namideo <namideo@gmail.com>
2023-07-31 21:28:58 -0400

    added header, footer, and portfolio components

---
+++ videoportfolio/src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+
+        <div className='social'>
+
+        <div><strong><a href="https://github.com/namideo" class="fa-brands fa-github-square" target="_blank"></a></strong></div>
+   
+        <div><strong><a href="https://www.linkedin.com/in/nick-amideo-767b3b39/" class="fa-brands fa-linkedin" target="_blank"></a></strong></div>
+        
+        <div><strong><a href="https://twitter.com/nikorasudo" class="fa-brands fa-twitter-square" target="_blank"></a></strong></div>
+
+        </div>
+   
+    </footer>
+
+    );
+
+}
+
+export default Footer;
---
+++ videoportfolio/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <div className='column name'>
+            <h1><strong>Nick Amideo</strong></h1>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
